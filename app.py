282685bdf50d2fab983df8b759c3bb421ec3b80e
galalmahmoud <galalengineer45@gmail.com>
2026-05-20 14:34:37 +0300

    fourth commit

--- app.py
+++ app.py
@@ -519,4 +519,5 @@ def grade_questions():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8085,host="0.0.0.0")
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
